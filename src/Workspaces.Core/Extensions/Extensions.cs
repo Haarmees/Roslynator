ba51d33b547437dedb681341bbcf9aebf5afe829
JochemH <jharmes@chipsoft.nl>
2022-07-14 14:35:15 +0200

    wip

--- src/Workspaces.Core/Extensions/Extensions.cs
+++ src/Workspaces.Core/Extensions/Extensions.cs
@@ -421,5 +421,30 @@ namespace Roslynator
             return reportDiagnostic != ReportDiagnostic.Suppress
                 && reportDiagnostic.ToDiagnosticSeverity() >= codeAnalysisOptions.SeverityLevel;
         }
+        
+        internal static IEnumerable<IEnumerable<int>> GroupConsecutiveNumbers(this IEnumerable<int> numbers)
+        {
+            var enumerable = numbers as int[] ?? numbers.ToArray();
+            if (!enumerable.Any())
+            {
+                return new List<IEnumerable<int>>();
+            }
+
+            var filteredNumbers = new List<IEnumerable<int>>();
+            var sequence = new List<int>() { enumerable.First()};
+            for (var i = 1; i < enumerable.Length; i++)
+            {
+                if (enumerable[i] - enumerable[i - 1] > 1)
+                {
+                    filteredNumbers.Add(sequence);
+                    sequence = new List<int>();
+                }
+                sequence.Add(enumerable[i]);
+            }
+            
+            filteredNumbers.Add(sequence);
+
+            return filteredNumbers;
+        }
     }
 }

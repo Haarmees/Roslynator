c9b4a52602a5fbc9c519a40a52a4fd96fcc69447
Josef Pihrt <josef.pihrt@outlook.com>
2023-07-16 12:25:39 +0200

    Add workflow (#1112)

--- src/Workspaces.Core/Extensions/Extensions.cs
+++ src/Workspaces.Core/Extensions/Extensions.cs
@@ -436,6 +436,9 @@ internal static class Extensions
         if (!codeAnalysisOptions.IsSupportedDiagnosticId(diagnostic.Id))
             return false;
 
+        if (diagnostic.Descriptor.CustomTags.Contains(WellKnownDiagnosticTags.Compiler))
+            return true;
+
         SyntaxTree tree = diagnostic.Location.SourceTree;
 
         ReportDiagnostic reportDiagnostic = (tree != null)
